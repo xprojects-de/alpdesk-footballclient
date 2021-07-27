6dd2436b5ef92b5b9b37862c9598a0f6e4d3b242
Benjamin Hummel <55982921+xprojects-de@users.noreply.github.com>
2021-07-27 14:58:17 +0200

    forms working

--- src/Library/Trainings/Trainings.php
+++ src/Library/Trainings/Trainings.php
@@ -28,6 +28,117 @@ class Trainings extends PluginBase
                 'target' => 'football_members',
                 'params' => []
             ]),
+            'form' => \json_encode([
+                'type' => 'form',
+                'target' => 'football_trainings',
+                'fields' => [
+                    [
+                        'type' => 'text',
+                        'value' => 'Hello World',
+                        'placeHolder' => 'Insert Value',
+                        'label' => 'Name',
+                        'name' => 'name',
+                        'mandatory' => true,
+                        'class' => 'w50'
+                    ],
+                    [
+                        'type' => 'text',
+                        'value' => 'Hello Description',
+                        'placeHolder' => 'Insert Value',
+                        'label' => 'Description',
+                        'name' => 'description',
+                        'class' => 'clr'
+                    ],
+                    [
+                        'type' => 'divider'
+                    ],
+                    [
+                        'type' => 'checkbox',
+                        'value' => true,
+                        'label' => 'MyCheckbox',
+                        'name' => 'mycheckbox'
+                    ],
+                    [
+                        'type' => 'date',
+                        'value' => '2021-01-01',
+                        'placeHolder' => 'Insert Value',
+                        'label' => 'Datum',
+                        'name' => 'date',
+                        'mandatory' => true,
+                        'class' => 'w50'
+                    ],
+                    [
+                        'type' => 'date',
+                        'value' => '2021-01-02',
+                        'placeHolder' => 'Insert Value',
+                        'label' => 'Datum2',
+                        'name' => 'date2',
+                        'class' => 'w50'
+                    ],
+                    [
+                        'type' => 'divider'
+                    ],
+                    [
+                        'type' => 'radio',
+                        'value' => 'Value2',
+                        'placeHolder' => '',
+                        'label' => 'MyRadio',
+                        'name' => 'myradio',
+                        'items' => [
+                            [
+                                'label' => 'Label1',
+                                'value' => 'Value1'
+                            ],
+                            [
+                                'label' => 'Label2',
+                                'value' => 'Value2'
+                            ]
+                        ]
+                    ],
+                    [
+                        'type' => 'select',
+                        'value' => 'Value4',
+                        'placeHolder' => '',
+                        'label' => 'MySelect',
+                        'name' => 'myselect',
+                        'items' => [
+                            [
+                                'label' => 'Label3',
+                                'value' => 'Value3'
+                            ],
+                            [
+                                'label' => 'Label4',
+                                'value' => 'Value4'
+                            ]
+                        ]
+                    ],
+                    [
+                        'type' => 'multiSelect',
+                        'value' => ['Value6'],
+                        'placeHolder' => '',
+                        'label' => 'MyMultiSelect',
+                        'name' => 'mymultiselect',
+                        'items' => [
+                            [
+                                'label' => 'Label5',
+                                'value' => 'Value5'
+                            ],
+                            [
+                                'label' => 'Label6',
+                                'value' => 'Value6'
+                            ]
+                        ]
+                    ],
+                    [
+                        'type' => 'divider'
+                    ],
+                    [
+                        'type' => 'submit',
+                        'label' => 'weg!',
+                        'name' => 'submitButton'
+                    ],
+                ]
+            ]),
             'reload' => $GLOBALS['TL_LANG']['alpdeskfootball_reload']
         ]);
     }
